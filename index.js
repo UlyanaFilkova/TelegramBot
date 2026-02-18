4e492724af30aaa8e73deaf6fbf2f1867866d456
Ulyana <filkova.ulyana@wiseweb.by>
2026-02-18 14:33:33 +0300

    feat: update versions

--- index.js
+++ index.js
@@ -6,11 +6,8 @@ dotenv.config();
 const token = process.env.BOT_TOKEN;
 
 if (!token) {
-    console.error('❌ Ошибка: BOT_TOKEN не найден в .env файле');
-    console.error('📝 Создай файл .env и добавь туда BOT_TOKEN=твой_токен');
-    process.exit(1); // Завершаем программу с ошибкой
+    console.error('❌ Ошибка: токен не найден ');
+    process.exit(1);
 }
 
 const bot = new TelegramBot(token, { polling: true });
-
-console.log('🤖 Бот запущен и готов к работе!');
