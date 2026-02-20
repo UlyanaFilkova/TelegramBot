578ef7f55c6cf1b1761b40931a36870a4d704570
Ulyana <filkova.ulyana@wiseweb.by>
2026-02-20 17:40:17 +0300

    feat: increase available AI models list

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,10 +1,29 @@
 export const FREE_MODELS = {
-    'deepseek-chat': 'openrouter/free',
+    // 'default-chat': 'openrouter/free',
 
     // быстрые
 
-    // Upstage
+    // upstage
     'solar-pro-3': 'upstage/solar-pro-3:free',
+
+    // arcee-ai
+    'arcee-ai-large': 'arcee-ai/trinity-large-preview:free',
+    'arcee-ai-mini': 'arcee-ai/trinity-mini:free',
+
+    // stepfun
+    'stepfun': 'stepfun/step-3.5-flash:free',
+
+    // z-ai
+    'z-ai': 'z-ai/glm-4.5-air:free',
+
+    // nvidia
+    'nvidia-nano': 'nvidia/nemotron-nano-12b-v2-vl:free',
+    'nvidia-3-nano': 'nvidia/nemotron-3-nano-30b-a3b:free',
+
+    // qwen
+    'qwen-30b': 'qwen/qwen3-vl-30b-a3b-thinking',
+    'qwen-235b': 'qwen/qwen3-vl-235b-a22b-thinking',
+    'qwen-235b-2507': 'qwen/qwen3-235b-a22b-thinking-2507',
 
     // OpenRouter (автоматически выбирает лучшую бесплатную модель)
     'free-router': 'openrouter/free',
@@ -15,7 +34,7 @@ export const FREE_MODELS = {
     'deepseek-r1': 'deepseek/deepseek-r1-0528:free',
 };
 
-export const DEFAULT_MODEL = "deepseek-chat"
+export const DEFAULT_MODEL = "nvidia-3-nano"//"default-chat"
 
 export const SYSTEM_PROMPT = {
     role: 'system',
